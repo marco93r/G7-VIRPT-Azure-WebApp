9839ab0025f5854e498a6f249fcf219cdc380d33
marco93r <marco.reeh93@gmail.com>
2025-11-22 14:40:50 +0100

    testing cd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hallo Azure! 👋</h1><p>Das ist meine erste Python Web App.</p>"
+    return "<h1>Hallo Azure! 👋</h1><p>Das ist meine erste Python Web App.</p><p>Das ist ein Test um das Continuous Deployment zu testen!</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
